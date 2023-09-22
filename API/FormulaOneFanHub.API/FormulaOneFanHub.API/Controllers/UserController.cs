9fa8f0059d8e84e9d503f3da2b12329a77a5825b
NithinMJose <116726866+NithinMJose@users.noreply.github.com>
2023-09-22 20:55:56 +0530

    jwt auth initial commit

--- API/FormulaOneFanHub.API/FormulaOneFanHub.API/Controllers/UserController.cs
+++ API/FormulaOneFanHub.API/FormulaOneFanHub.API/Controllers/UserController.cs
@@ -2,6 +2,10 @@
 using FormulaOneFanHub.API.DTO;
 using FormulaOneFanHub.API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace FormulaOneFanHub.API.Controllers
 {
@@ -10,21 +14,53 @@ namespace FormulaOneFanHub.API.Controllers
     public class UserController : ControllerBase
     {
         private readonly FormulaOneFanHubContxt _fanHubContext;
-        public UserController(FormulaOneFanHubContxt fanHubContxt)
+        private readonly IConfiguration  _config;
+
+        public UserController(FormulaOneFanHubContxt fanHubContxt, IConfiguration configuration)
         {
             _fanHubContext = fanHubContxt;
+            _config = configuration;
         }
+
         [HttpPost("Login")]
         public IActionResult Authenticate(LoginDto loginDto)
         {
-            User user = _fanHubContext.Users.SingleOrDefault(x => x.UserName == loginDto.UserName && x.Password == loginDto.Password);
+            User user = _fanHubContext.Users.FirstOrDefault(x => x.UserName == loginDto.UserName && x.Password == loginDto.Password);
             if (user == null)
             {
                 return Unauthorized();
             }
-            return Ok(loginDto);
-
+            var token = GenerateToken(user);
+            // Return a JSON response with token and success:true
+            return Ok(new { token = token, success = true });
         }
+
+        private string GenerateToken(User user)
+        {
+            try
+            {
+                var claims = new[] { new Claim(ClaimTypes.Name, user.UserName) };
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: _config["Jwt:Issuer"],
+                    audience: _config["Jwt:Audience"],
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(15), // Adjust the expiration time as needed
+                    signingCredentials: credentials
+                );
+
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception (e.g., log it) and return null or an error message
+                return null;
+            }
+        }
+
         [HttpPost("Register")]
         public IActionResult Register(UserDto userDto)
         {
@@ -32,6 +68,12 @@ namespace FormulaOneFanHub.API.Controllers
             {
                 return BadRequest(ModelState);
             }
+
+            if (userDto.Password != userDto.ConfirmPassword)
+            {
+                return BadRequest("Passwords do not match");
+            }
+
             var clientRole = _fanHubContext.Roles.SingleOrDefault(x => x.RoleName == "Client");
             User userToCreate = new User
             {
@@ -46,8 +88,9 @@ namespace FormulaOneFanHub.API.Controllers
             };
             _fanHubContext.Users.Add(userToCreate);
             _fanHubContext.SaveChanges();
-            return StatusCode(201);
 
+            // Return a JSON response with success:true
+            return Ok(new { success = true });
         }
 
         [HttpPost("RegisterAdmin")]
@@ -72,7 +115,13 @@ namespace FormulaOneFanHub.API.Controllers
             _fanHubContext.Users.Add(userToCreate);
             _fanHubContext.SaveChanges();
             return StatusCode(201);
+        }
 
+        [HttpGet("GetAllUsers")]
+        public IActionResult GetAllUsers()
+        {
+            List<User> users = _fanHubContext.Users.ToList();
+            return Ok(users);
         }
     }
 }
--- API/FormulaOneFanHub.API/FormulaOneFanHub.API/Program.cs
+++ API/FormulaOneFanHub.API/FormulaOneFanHub.API/Program.cs
@@ -1,10 +1,29 @@
 using FormulaOneFanHub.API.Data;
+using FormulaOneFanHub.API.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//JWT token config.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+        };
+    }
+    );
 
+// Add services to the container.
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<FormulaOneFanHubContxt>(options =>
@@ -14,6 +33,18 @@ builder.Services.AddDbContext<FormulaOneFanHubContxt>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Define CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost3000", builder =>
+    {
+        builder
+            .WithOrigins("http://localhost:3000") // Allow requests from this origin
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,6 +53,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionHandlerMiddleware>(); // Global exception handling
+
+// Enable CORS
+app.UseCors("AllowLocalhost3000"); // Enable CORS policy
 
 app.UseHttpsRedirection();
 
